f9759673b0a317524adecf12e4d3adc474fd95f8
Joshua Whitley <jwhitley@autonomoustuff.com>
2019-05-13 10:39:32 -0500

    Pretty sure I have canlib done.

--- package-canlib.sh
+++ package-canlib.sh
@@ -40,34 +40,50 @@ mv linuxcan/ kvaser-canlib/
 VERSION=$(cat kvaser-canlib/moduleinfo.txt | grep version | sed -e "s/version=//" -e "s/_/./g" -e "s/\r//g")
 DEBIAN_VERSION=${VERSION}-0ubuntu0~ppa
 
-# Strip down to only canlib
-cd kvaser-canlib/
-rm 10-kvaser.rules
-rm -r leaf/ linlib/ mhydra/ pcican/ pcican2/ pciefd/ usbcanII/ virtualcan/ 
-cp ../Makefile-canlib Makefile
-
 if [ $# -gt 0 ]; then
   DEBIAN_VERSION=${DEBIAN_VERSION}$1
 else
   DEBIAN_VERSION=${DEBIAN_VERSION}0
 fi
 
+# Strip down to only canlib
+cd kvaser-canlib/
+rm 10-kvaser.rules
+rm -r leaf/ linlib/ mhydra/ pcican/ pcican2/ pciefd/ usbcanII/ virtualcan/ 
+cp ../Makefile-canlib Makefile
+
+# Modify Makefiles for DEB install
+sed -i "1s/^/prefix = \/usr\n/" canlib/Makefile
+sed -i "s/install \(.*\)\/usr/install -D \1\$(DESTDIR)\$(prefix)/g" canlib/Makefile
+sed -i "s/mkdir \(.*\)\/usr/mkdir \1\$(DESTDIR)\$(prefix)/g" canlib/Makefile
+sed -i "s/cp \(.*\)\/usr/cp \1\$(DESTDIR)\$(prefix)/g" canlib/Makefile
+sed -i "s/ln \(.*\)\/usr/ln \1\$(DESTDIR)\$(prefix)/g" canlib/Makefile
+# Have to run 3 times due to combined "rm" lines
+sed -i "s/rm \(.*\)\/usr/rm \1\$(DESTDIR)\$(prefix)/g" canlib/Makefile
+sed -i "s/rm \(.*\)\/usr/rm \1\$(DESTDIR)\$(prefix)/g" canlib/Makefile
+sed -i "s/rm \(.*\)\/usr/rm \1\$(DESTDIR)\$(prefix)/g" canlib/Makefile
+sed -i "s/^.*\/sbin\/ldconfig.*$//g" canlib/Makefile
+
+sed -i "1s/^/prefix = \/usr\n/" canlib/examples/Makefile
+sed -i "s/mkdir \(.*\)\/usr/mkdir \1\$(DESTDIR)\$(prefix)/g" canlib/examples/Makefile
+sed -i "s/cp \(.*\)\/usr/cp \1\$(DESTDIR)\$(prefix)/g" canlib/examples/Makefile
+sed -i "s/rm \(.*\)\/usr/rm \1\$(DESTDIR)\$(prefix)/g" canlib/examples/Makefile
+
 # Make source tarball
-cd ..
-tar zcf kvaser-canlib-dev_${VERSION}.orig.tar.gz kvaser-canlib/
+tar zcf ../kvaser-canlib-dev_${VERSION}.orig.tar.gz ../kvaser-canlib/
 
 # Modify debian files with correct OS and package versions
-cp -r canlib-debian/ kvaser-canlib/debian/
-sed -i "s/unstable/${OS_VER}/" kvaser-canlib/debian/changelog
-sed -i "s/MODULE_VERSION/${DEBIAN_VERSION}/" kvaser-canlib/debian/changelog
-sed -i "s/DATE_STAMP/$(LC_ALL=C date -R)/" kvaser-canlib/debian/changelog
-sed -i "s/DATE_STAMP/$(LC_ALL=C date -R)/" kvaser-canlib/debian/README.Debian
+cp -r ../canlib-debian/ debian/
+sed -i "s/unstable/${OS_VER}/" debian/changelog
+sed -i "s/MODULE_VERSION/${DEBIAN_VERSION}/" debian/changelog
+sed -i "s/DATE_STAMP/$(LC_ALL=C date -R)/" debian/changelog
+sed -i "s/DATE_STAMP/$(LC_ALL=C date -R)/" debian/README.Debian
 
 # Build the package
-cd kvaser-canlib
 debuild -S
+cd ..
 
 # Upload
-#dput ppa:jwhitleyastuff/kvaser-linuxcan kvaser-canlib-dev_${DEBIAN_VERSION}_source.changes
+dput ppa:jwhitleyastuff/kvaser-linuxcan kvaser-canlib-dev_${DEBIAN_VERSION}_source.changes
 echo ""
 echo "Done!"
